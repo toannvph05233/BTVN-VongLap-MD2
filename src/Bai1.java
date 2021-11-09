5b9dd1dd382f135878c58689e6294306663543fc
toannvph05233 <johntoan98@gmail.com>
2021-11-09 09:18:57 +0700

    up

--- src/Bai1.java
+++ src/Bai1.java
@@ -1,15 +1,17 @@
 public class Bai1 {
+    int a;
+
     public static void main(String[] args) {
 //  Bài 1: Tìm số chia hết cho 5 tại vị trí thứ 20, bắt đầu từ 11.
         int count = 0;
         int n = 11;
-        while (count<20){
-            if (n%5 == 0){
-                count ++;
+        while (count < 20) {
+            if (n % 5 == 0) {
+                count++;
             }
             n++;
         }
 
-        System.out.println(n-1);
+        System.out.println(n - 1);
     }
 }
---
+++ src/KiemTraChuoi.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class KiemTraChuoi {
+    public static void main(String[] args) {
+        String str = "abczy zabc";
+        String[] arr = str.split("");
+        System.out.println(Arrays.toString(arr));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập phần tử cần tìm");
+        String str2 = sc.nextLine();
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(str2)) {
+                count++;
+                System.out.println(str2 + " ở vị trí thứ " + i);
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(Arrays.toString(removeArr(arr,str2,count)));
+
+    }
+
+    public static String[] removeArr(String[] arr, String value, int sl) {
+        String[] arrNew = new String[arr.length - sl];
+        int count = 0;
+        for (int i = 0; i < arr.length - count; i++) {
+            if (arr[i+count].equals(value)) {
+                count++;
+            }
+            arrNew[i] = arr[i + count];
+
+        }
+        return arrNew;
+    }
+
+}
